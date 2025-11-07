3da5f8f8cc2257d2ee4b10f4398d33746ecda857
SchoolHTMLDeveloper <brahay4221@students.davidson.k12.nc.us>
2025-11-07 12:52:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+ cv ddeddeeeimport express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import fs from "fs";
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const ADMIN_PASSWORDS = [process.env.ADMIN_PASSWORD, process.env.COADMIN];
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 // Paths
 const BANNED_WORDS_FILE = path.join(__dirname, "bannedwords.json");
